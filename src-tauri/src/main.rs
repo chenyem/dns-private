99c96ca8496c966dc3640bf36841413ad3c4e2e1
chenyem <168181847+chenyem@users.noreply.github.com>
2024-04-28 17:40:44 +0800

    update v.0.0.19

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -1,15 +1,26 @@
 // Prevents additional console window on Windows in release, DO NOT REMOVE!!
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
 
-// Learn more about Tauri commands at https://tauri.app/v1/guides/features/command
+use tauri::AppHandle;
+
+mod utils;
 #[tauri::command]
-fn greet(name: &str) -> String {
-    format!("Hello, {}! You've been greeted from Rust!", name)
+fn greet(app: AppHandle, name: &str) -> String {
+  format!(
+    "Hello, {}! from Rust! and veriosn is @@ {} @@",
+    name,
+    app.package_info().version
+  )
 }
 
 fn main() {
-    tauri::Builder::default()
-        .invoke_handler(tauri::generate_handler![greet])
-        .run(tauri::generate_context!())
-        .expect("error while running tauri application");
+  tauri::Builder::default()
+    .setup(|app| {
+      let app_handle = app.handle();
+      tauri::async_runtime::spawn(async { utils::run_check_update(app_handle, false, None) });
+      Ok(())
+    })
+    .invoke_handler(tauri::generate_handler![greet])
+    .run(tauri::generate_context!())
+    .expect("error while running tauri application");
 }
